d07f71cda5bfa5b48cc4fa93cc3f4b3dba29dd0b
ankit <ankit.nandal@globallogic.com>
2018-09-09 14:40:06 +0530

    Initial Commit

---
+++ Photoz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photoz
+//
+//  Created by Ankit Nandal on 09/09/18.
+//  Copyright © 2018 Ankit Nandal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
